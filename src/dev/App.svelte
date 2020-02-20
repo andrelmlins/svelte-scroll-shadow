a968616a0254cc097d89eaedf814f79fc984eff5
André Lins <andrelucas01@hotmail.com>
2020-02-20 11:35:38 -0300

    Creating component

---
+++ src/dev/App.svelte
@@ -0,0 +1,39 @@
+<script>
+  import SvelteScrollShadow from "../lib/index.svelte";
+</script>
+
+<style>
+  main {
+    align-items: center;
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    width: 100%;
+  }
+
+  div {
+    align-items: center;
+    background-color: #417b5a;
+    display: flex;
+    flex-flow: column wrap;
+    height: 120px;
+    justify-content: center;
+    margin: 10px;
+    width: 300px;
+  }
+
+  h1 {
+    color: #1f2041;
+    text-transform: uppercase;
+  }
+</style>
+
+<main>
+  <h1>Svelte Scroll Shadow</h1>
+  <SvelteScrollShadow let:onRequest let:onExit>
+    <div>
+      <button on:click={() => onRequest()}>FullScreen</button>
+      <button on:click={() => onExit()}>Screen</button>
+    </div>
+  </SvelteScrollShadow>
+</main>
---
+++ src/lib/index.svelte
@@ -0,0 +1,84 @@
+<script>
+  import { onMount, onDestroy, createEventDispatcher } from "svelte";
+  import ResizeObserver from "resize-observer-polyfill";
+  import screenfull from "screenfull";
+
+  export let style = "";
+  export let styleSubcontainer;
+  export let scrollColor = "#c5c5c5";
+  export let scrollPadding = "0px";
+  export let scrollWidth = "8px";
+  export let scrollColorHover = "#a6a6a6";
+  export let shadow =
+    "0 2px 4px rgba(0, 0, 0, 0.2) inset, 0 -2px 4px rgba(0, 0, 0, 0.2) inset";
+  export let isShadow = false;
+
+  $: shadow = isShadow ? shadow : "none";
+
+  let scroll = false;
+  let component = null;
+
+  onMount(() => {
+    RO = new ResizeObserver(() => {
+      if (
+        component.parentNode.clientHeight < component.parentNode.scrollHeight
+      ) {
+        scroll = true;
+      } else {
+        scroll = false;
+      }
+    });
+    RO.observe(component.parentNode);
+  });
+
+  onDestroy(() => {
+    RO.disconnect();
+  });
+</script>
+
+<style>
+  .containerScroll {
+    box-shadow: var(--shadow);
+  }
+  .container {
+    width: 100%;
+    overflow-y: hidden;
+    box-sizing: border-box;
+    display: flex;
+  }
+  .subcontainer {
+    padding-right: var(--scrollPadding);
+    overflow-y: auto;
+    flex: 1;
+    scrollbar-color: var(--scrollColor);
+    scrollbar-width: thin;
+  }
+  .subcontainer::-webkit-scrollbar {
+    width: var(--scrollWidth);
+    background: transparent;
+  }
+  .subcontainer::-webkit-scrollbar-track {
+    background: "transparent";
+  }
+  .subcontainer::-webkit-scrollbar-thumb {
+    background: var(--scrollColor);
+    border-radius: 5;
+    overflow: hidden;
+  }
+  .subcontainer::-webkit-scrollbar-thumb:hover {
+    background: var(--scrollColorHover);
+  }
+</style>
+
+<div
+  aria-label="container-scroll"
+  style="{style}--scrollColor:{scrollColor};--shadow:{shadow};--scrollPadding:{scrollPadding};--scrollWidth:{scrollWidth};--scrollColorHover:{scrollColorHover}"
+  class={`container ${scroll ? 'containerScroll' : ''}`}>
+  <div
+    aria-label="subcontainer-scroll"
+    bind:this={component}
+    className="subcontainer"
+    style={styleSubcontainer}>
+    <slot />
+  </div>
+</div>
